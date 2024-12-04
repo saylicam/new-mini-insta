c4e45b3cc6ee99a8f35d0d3de347454eeb01d6cc
Ilyas <ilyas.nassiri@ifosup.wavre.be>
2024-12-04 19:13:41 +0100

    fini

--- database/migrations/2024_12_02_181125_add_image_path_to_posts_table.php
+++ database/migrations/2024_12_02_181125_add_image_path_to_posts_table.php
@@ -4,27 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+// return new class extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      */
 
-    /**
-     * Reverse the migrations.
-     */
-    public function up()
-    {
-        Schema::table('posts', function (Blueprint $table) {
-            $table->string('image_path')->nullable()->after('content');
-        });
-    }
+//     /**
+//      * Reverse the migrations.
+//      */
+//     public function up()
+//     {
+//         Schema::table('posts', function (Blueprint $table) {
+//             $table->string('image_path')->nullable()->after('content');
+//         });
+//     }
 
-    public function down()
-    {
-        Schema::table('posts', function (Blueprint $table) {
-            $table->dropColumn('image_path');
-        });
-    }
+//     public function down()
+//     {
+//         Schema::table('posts', function (Blueprint $table) {
+//             $table->dropColumn('image_path');
+//         });
+//     }
 
-};
+// };
